746d46bea8ce52e90a93a89e67f288c5e78efe56
Dan L <DJL.Consulting@outlook.com>
2022-08-21 14:22:14 +0400

    Added bullet data

--- ballist-web/src/index.js
+++ ballist-web/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {bulletData} from './Bullets.js';
 import {BallistCalc, RangeData} from './Ballist.js'
 import {Load} from './Ballist.js'
 //import {RangeData} from './Ballist.js'
@@ -8,7 +9,7 @@ import {Load} from './Ballist.js'
 import reportWebVitals from './reportWebVitals';
 
 class Form extends React.Component {
-  state = new Load();
+  state =  new Load();
   handleSubmit = async (event) => {
     event.preventDefault();  // disable default
 
@@ -33,15 +34,111 @@ class Form extends React.Component {
       }
     }
     this.props.onSubmit(bData);
-
-    //this.setState( { userName: '' });
   }
-  render() {    
+
+  closeBulletBox()
+  {
+    this.setState( {ShowBulletPicker:false});
+  }
+
+  getDropdowns()
+  {
+    //const unique = [...new Set(bulletData.map(item => item.Caliber))];
+    this.setState( { Calibers: [...new Set(bulletData.map(item => item.Caliber))] } );
+    this.setState( { Brands: [...new Set(bulletData.map(item => item.Manufacturer))] } );
+    var bc = document.getElementById("BCbox").getBoundingClientRect();
+    const bbb1 = document.getElementById("Bulletbox").getBoundingClientRect();  
+    const bb = document.getElementById("Bulletbox");
+
+    //console.log(bb);
+
+    //console.log("BCR "+ bc.right);
+    console.log("BB1 t/b "+bbb1.top+' -- '+bbb1.bottom);
+    
+
+    //bb.style.position = "absolute";
+    
+    this.setState({l: bc.right + 20, t: bc.top - (bbb1.height/2)});
+
+    //bb.style.left = (4 * bc.right.toString) + "px"; // bc.right; // 2000;// bc.style.left + bc.offsetWidth; //+ bc.width;
+    //bb.style.top = bc.top.toString + "px"; //(bc.top+bc.bottom); ///2;
+    const bbb2 = document.getElementById("Bulletbox").getBoundingClientRect();    
+    console.log("BB2 t/b "+bbb2.top+' -- '+bbb2.bottom);    
+    this.setState( { ShowBulletPicker:true }); 
+   }
+
+   setCaliber(cal)
+   {
+      this.setState({ Caliber: cal });
+      this.getBullets(cal, this.state.Brand);
+   }
+
+   setBrand(brand)
+   {
+    this.setState({ Brand: brand });
+    this.getBullets(this.state.Caliber, brand);
+   }
+
+   getBullets(cal, brand)
+   {
+    var brandBullets = (brand == "" ? bulletData : bulletData.filter(({ Manufacturer }) => Manufacturer.includes(brand)));
+    this.setState( { Bullets: (cal == "" ? brandBullets : brandBullets.filter(({ Caliber }) => Caliber.includes(cal))) });   
+    //console.log(bullets);
+
+   }
+
+  render() {   
     return (
       <center>
       <div class="container">
       <h3>Load Data</h3>
       <form onSubmit={this.handleSubmit} action="">
+
+      <div id="Bulletbox" class="bulletPicker" style={{ visibility: (this.state.ShowBulletPicker == true ?  "visible" : "hidden"), left: this.state.l, top: this.state.t }}>
+       <center><h3>Bullet Picker</h3></center>
+       <div class="row">
+        <div className="col-15">
+          <label><b>Caliber</b></label> 
+        </div>
+        <div className="col-35">
+          <select onChange={ event => this.setCaliber(event.target.value) }>
+          <option selected="selected" disabled="disabled">Choose Caliber...</option>
+            {this.state.Calibers.map((option) => (
+              <option value={option}>{option}</option>
+            ))}
+          </select>
+        </div>
+       </div>
+
+       <div class="row">
+        <div className="col-15">
+          <label><b>Brand</b></label> 
+        </div>
+        <div className="col-35"> 
+          <select onChange={ event => this.setBrand(event.target.value) }>
+          <option selected="selected" disabled="disabled">Choose Brand...</option>
+            {this.state.Brands.map((option) => (
+              <option value={option}>{option}</option>
+            ))}
+          </select>
+        </div>
+        </div>
+
+        <div class="row">
+        <div className="col-15">
+          <label><b>Bullet</b></label> 
+        </div>
+        <div className="col-75"> 
+          <select onChange={ event => this.setState({ BallisticCoefficient: this.state.Bullets[event.target.value].BC, BulletWeight: this.state.Bullets[event.target.value].Weight, ShowBulletPicker: false }) }>
+          <option selected="selected" disabled="disabled">Choose Bullet...</option>
+            {this.state.Bullets.map((option, index) => (
+              <option value={index}>{(this.state.Caliber == "" ? option.Caliber+ ' cal ': '') + (this.state.Brand == "" ? option.Manufacturer+ ' ': '') + option.Bullet+' '+option.Weight+' Grain ' +option.BC+' BC'}</option>
+            ))}
+          </select>
+        </div>
+        </div>
+<br />
+      </div>      
 
       <div class="row">
       <div class="col-25" />
@@ -51,7 +148,8 @@ class Form extends React.Component {
       <div class="col-10">
       <input type="text" placeholder="Velocity" 
                value={this.state.MuzzleVelocity} 
-               onChange={ event => this.setState({ MuzzleVelocity: event.target.value })} 
+               onChange={ event => this.setState({ MuzzleVelocity: event.target.value })}
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -66,9 +164,10 @@ class Form extends React.Component {
         <label><b>Ballistic Coefficient</b></label>
       </div>
       <div class="col-10">
-      <input type="text" placeholder="BC" 
+      <input type="text" id="BCbox" 
                value={this.state.BallisticCoefficient} 
                onChange={ event => this.setState({ BallisticCoefficient: event.target.value })} 
+               onFocus={ event => this.getDropdowns() }
                required />
       </div>
       <div class="col-5" />
@@ -86,6 +185,7 @@ class Form extends React.Component {
       <input type="text" placeholder="BC" 
                value={this.state.BulletWeight} 
                onChange={ event => this.setState({ BulletWeight: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -103,6 +203,7 @@ class Form extends React.Component {
       <input type="text" placeholder="Range" 
                value={this.state.SightInRange} 
                onChange={ event => this.setState({ SightInRange: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -120,6 +221,7 @@ class Form extends React.Component {
       <input type="text" placeholder="Hits" 
                value={this.state.SightInHeight} 
                onChange={ event => this.setState({ SightInHeight: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -137,6 +239,7 @@ class Form extends React.Component {
       <input type="text" placeholder="Height" 
                value={this.state.SightHeight} 
                onChange={ event => this.setState({ SightHeight: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -154,6 +257,7 @@ class Form extends React.Component {
       <input type="text" placeholder="Max" 
                value={this.state.MaxRange} 
                onChange={ event => this.setState({ MaxRange: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required />
       </div>
       <div class="col-5" />
@@ -171,6 +275,7 @@ class Form extends React.Component {
       <input type="text" placeholder="Increment" 
                value={this.state.RangeIncrement} 
                onChange={ event => this.setState({ RangeIncrement: event.target.value })} 
+               onFocus={ event => this.closeBulletBox() }
                required /> 
       </div>
       <div class="col-5" />
@@ -267,10 +372,10 @@ class App extends React.Component {
     ld.SightHeight = 2.0;
     ld.SightInRange = 100;
     ld.SightHeight = 1.5;
-   
+
     return (
       <div>
-        <center><h1><b>Ballistics Calculator</b></h1> </center>
+        <center><h1><font color='#dddddd'><b>Ballistics Calculator</b></font></h1> </center>
         <table>
           <tr>
             <td width="3%"></td>
